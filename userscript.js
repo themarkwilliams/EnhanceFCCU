14ee67c0adba134271da0811b9c7e629e78796bc
themarkwilliams <themarkwilliams@users.noreply.github.com>
2025-03-07 23:14:32 -0600

    Update userscript.js
    Blocking the chatbot

--- userscript.js
+++ userscript.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name Enhance FCCU
-// @version 1.2
+// @version 1.3
 // @author Mark Williams
 // @description "Fix FCCU Changes"
 // @match        https://www.firstcommunityexpressnet.com/*
@@ -18,4 +18,5 @@ GM_addStyle('.amount-credit-column { font-weight:bold !important; }');// 3/10/18
 
 // Hide the Chat bubble
 GM_addStyle('#ncr-cb-chat-win-trigger { display:none !important; }'); // 7/14/24
-GM_addStyle('.ncr-cb-chat-win-notif-section-popping { display:none !important; }'); // 7/14/24
+GM_addStyle('#ncr-cb-chat-win-notif-section-popping { display:none !important; }'); // 7/14/24
+GM_addStyle('#ncr-cb-chat-win-notif-section { display:none !important; }'); // 3/7/25
